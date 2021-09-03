d616b59d44b893fa284a6a132dcb4471e65cfe9d
KirkFox <kirkTWM.MC@gmail.com>
2021-09-02 23:09:28 -0700

    Fixed DifficultyChangeEvent

--- src/main/java/me/kirkfox/idifficulty/event/DifficultyChangeEvent.java
+++ src/main/java/me/kirkfox/idifficulty/event/DifficultyChangeEvent.java
@@ -1,12 +1,24 @@
 package me.kirkfox.idifficulty.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
 public abstract class DifficultyChangeEvent extends PlayerEvent {
 
+    private static final HandlerList HANDLERS = new HandlerList();
+
     public DifficultyChangeEvent(Player p) {
         super(p);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
     }
 
 }
