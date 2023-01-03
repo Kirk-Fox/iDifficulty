9635d20b020920013ddbc8b78231fc700b9f78d4
Kirk-Fox <kirkTWM.MC@gmail.com>
2023-01-02 16:38:25 -0800

    Improvements to config updater

--- src/main/java/me/kirkfox/idifficulty/ConfigHandler.java
+++ src/main/java/me/kirkfox/idifficulty/ConfigHandler.java
@@ -2,7 +2,6 @@ package me.kirkfox.idifficulty;
 
 import me.kirkfox.idifficulty.difficulty.Difficulty;
 import me.kirkfox.idifficulty.difficulty.DifficultyHandler;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,7 +9,6 @@ import java.util.*;
 
 public class ConfigHandler {
 
-    private static final int CONFIG_VERSION = 1;
     private static final String[][] CONFIG_VALUES = {{"keep-inventory", "keepInv"}, {"keep-xp", "keepExp"},
             {"mob-xp-multiplier", "mobExpMod"}, {"ore-xp-multiplier", "oreExpMod"}, {"damage-multiplier", "damageMod"},
             {"mob-doubled-loot-chance", "mobLootChance"}, {"ore-doubled-loot-chance", "oreLootChance"},
@@ -19,7 +17,6 @@ public class ConfigHandler {
 
     private static JavaPlugin plugin;
     private static FileConfiguration config;
-    private static ConfigurationSection diffConfig;
     private static Set<String> diffKeys;
 
     public static void registerConfig() {
@@ -37,44 +34,75 @@ public class ConfigHandler {
 
     private static void enableConfig() {
         config = plugin.getConfig();
-        diffConfig = Objects.requireNonNull(config.getConfigurationSection("difficulties"));
-        diffKeys = diffConfig.getKeys(false);
-        if(!config.contains("config-version", true)) {
-            config.createSection("config-version");
-            config.set("config-version", CONFIG_VERSION);
-
-            Objects.requireNonNull(config.getConfigurationSection("toggle")).set("min-health-starvation", false);
-
-            updateMobOreConfig("toggle", "xp-multiplier");
-            updateMobOreConfig("toggle", "doubled-loot-chance");
-            for (String key : diffKeys) {
-                updateMobOreConfig("difficulties." + key, "xp-multiplier");
-                updateMobOreConfig("difficulties." + key, "doubled-loot-chance");
-            }
-
-            addToConfig("min-health-starvation", 10);
-            addToConfig("requires-permission", false);
-
-            plugin.saveConfig();
-            IDifficulty.outputLog("Config updated!");
-        }
+        diffKeys = Objects.requireNonNull(config.getConfigurationSection("difficulties")).getKeys(false);
+        updateConfig();
 
         for(String[] v : CONFIG_VALUES) {
             TOGGLE_MAP.put(v[1], config.getBoolean("toggle." + v[0]));
         }
     }
 
-    private static void addToConfig(String path, Object value) {
-        for (String key : diffKeys) {
-            diffConfig.set(key + "." + path, value);
+    private static void updateConfig() {
+        int configVersion = config.getInt("config-version", 0);
+        if(configVersion == 0) {
+            setWithComment("config-version", 1, "DO NOT CHANGE UNDER ANY CIRCUMSTANCES");
+
+            updateMobOreConfig("toggle", "xp-multiplier", true, "If \"true\" difficulties " +
+                    "will affect how much experience %s drop", "mobs", "ores");
+            updateMobOreConfig("toggle", "doubled-loot-chance", true, "If \"true\" difficulties " +
+                    "will have a chance to double item drops from %s", "mobs", "ores");
+            for (String key : diffKeys) {
+                updateMobOreConfig("difficulties." + key, "xp-multiplier", key.equals("easy"),
+                        "Sets the multiplier of experience gained from %s", "killing mobs",
+                        "mining blocks and using furnaces");
+                updateMobOreConfig("difficulties." + key, "doubled-loot-chance", key.equals("easy"),
+                        "Sets the chance (where 0.0 is 0%% and 1.0 is 100%%) that %s by a player will drop double items",
+                        "mobs killed", "ores broken");
+            }
+
+            setWithComment("toggle.min-health-starvation", false, "If \"true\" difficulties will " +
+                    "affect the health at which a player will stop taking damage from starvation");
+
+            addToConfig("min-health-starvation", 10, "Sets the health at which starvation stops " +
+                    "dealing damage to the player where 20 is 10 hearts and 0 is 0 hearts");
+            addToConfig("requires-permission", false, Arrays.asList("Sets whether this difficulty requires " +
+                            "players that use it to have the \"idifficulty.diff.[difficulty_name]\" permission",
+                    "For example, if set to true for this difficulty, players would need the permission node " +
+                            "\"idifficulty.diff.easy\" to use it"));
+
+            plugin.saveConfig();
+            IDifficulty.outputLog("Config updated!");
         }
     }
 
-    private static void updateMobOreConfig(String key, String path) {
+    private static void addToConfig(String path, Object value, List<String> comment) {
+        for (String key : diffKeys) {
+            setWithComment("difficulties." + key + "." + path, value, key.equals("easy"), comment);
+        }
+    }
+
+    private static void addToConfig(String path, Object value, String comment) {
+        addToConfig(path, value, Collections.singletonList(comment));
+    }
+
+    private static void updateMobOreConfig(String key, String path, boolean addComment, String comment, String mob, String ore) {
         Object mod = config.get(key + "." + path);
         config.set(key + "." + path, null);
-        config.set(key + ".mob-" + path, mod);
-        config.set(key + ".ore-" + path, mod);
+        setWithComment(key + ".mob-" + path, mod, addComment, String.format(comment, mob));
+        setWithComment(key + ".ore-" + path, mod, addComment, String.format(comment, ore));
+    }
+
+    private static void setWithComment(String path, Object value, boolean addComment, List<String> comment) {
+        config.set(path, value);
+        if(addComment) config.setComments(path, comment);
+    }
+
+    private static void setWithComment(String path, Object value, boolean addComment, String comment) {
+        setWithComment(path, value, addComment, Collections.singletonList(comment));
+    }
+
+    private static void setWithComment(String path, Object value, String comment) {
+        setWithComment(path, value, true, comment);
     }
 
     public static Map<String, Boolean> getToggleMap() {
